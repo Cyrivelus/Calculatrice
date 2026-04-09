189e639acd684bb735f3cde2b9ae92d4752ac936
Cyrivelus <cyrivelmars2013@gmail.com>
2026-04-09 12:11:29 +0100

    feat: version finale calculatrice

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const resultat = document.getElementById('resultat');
 const modeButtons = document.querySelectorAll('.mode');
 const calcButtons = document.querySelectorAll('.standard .boutons button');
@@ -135,24 +134,3 @@ pretButton.addEventListener('click', () => {
 });
 
 setMode('standard');
-=======
-let resultat = document.getElementById('resultat');
-let boutons = document.querySelectorAll('button');
-
-boutons.forEach(bouton => {
-    bouton.addEventListener('click', () => {
-        let valeur = bouton.textContent;
-        if(valeur === 'C') {
-            resultat.value = '';
-        } else if (valeur === '=') {
-            try {
-                resultat.value = eval(resultat.value);
-            } catch(error) {
-                resultat.value = 'Erreur';
-            }
-        } else {
-            resultat.value += valeur;
-        }
-    });
-});
->>>>>>> c6170944f6902c81551bdb3bea2c6518f633d328
